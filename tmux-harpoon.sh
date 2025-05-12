4abea40e37fdd8bc44e309e195e0e622f8c90e96
WillssEdgar <willssedgar2123@gmail.com>
2025-05-12 07:46:26 -0400

    initial commit

---
+++ tmux-harpoon.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+switch() {
+	selected=$1
+	line_count=$(wc -l < ~/bin/tmux-harpoon/pane-list)
+
+	if [ "$line_count" -ge "$selected" ]; then
+		session=$(sed -n "${selected}p" ~/bin/tmux-harpoon/pane-list)
+		tmux switch -t "${session}"
+	else
+		echo "There is not a session available"
+	fi
+}
+
+add() {
+	piped_content=$(cat)
+	already_contains=false
+
+	while IFS= read -r line; do
+		if [ "$line" = "$piped_content" ]; then
+			already_contains = true
+		fi
+	done < ~/bin/tmux-harpoon/pane-list
+
+	if [ "$already_contains" = false ]; then
+		echo "$piped_content" >> ~/bin/tmux-harpoon/pane-list
+	fi
+}
+
+view() {
+	tmux display-popup -d "~/bin/tmux-harpoon/" -E -- sh -c 'vim "$@"' -- ~/bin/tmux-harpoon/pane-list
+}
+
+help() {
+	echo "figure it out"
+}
+
+main() {
+	while getopts ":haAr:R:d:ls:e" opt; do
+		case "$opt" in
+			h | :) help && exit 0 ;;
+			a) add ;;
+			s) switch "$OPTARG" ;;
+			l) view ;;
+		esac
+	done
+}
+
+main "$@"
